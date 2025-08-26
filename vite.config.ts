ef0b03360857e4af89f180db0cee28b957ede357
kcswimrac <kcswimrac@gmail.com>
2025-08-26 12:05:43 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/sb1-jpdein7l/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
