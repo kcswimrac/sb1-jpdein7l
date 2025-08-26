fa385ca5cee2100fb32815d19644df8287faf117
kcswimrac <kcswimrac@gmail.com>
2025-08-26 12:33:35 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/sb1-jpdein7l',
+  base: '/sb1-jpdein7l/',
   // Remove the base path for universal compatibility
   optimizeDeps: {
     exclude: ['lucide-react'],
