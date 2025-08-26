65ad0187818c30308a50d425bb477578c544a779
kcswimrac <kcswimrac@gmail.com>
2025-08-26 12:37:23 -0700

    Update HeroSection.tsx
    scroll indicator

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -61,7 +61,7 @@ const HeroSection = () => {
         </div>
 
         {/* Scroll indicator */}
-        <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 animate-bounce">
+        <div className="absolute bottom-2 left-1/2 transform -translate-x-1/2 animate-bounce">
           <div className="w-6 h-10 border-2 border-energy-green rounded-full flex justify-center">
             <div className="w-1 h-3 bg-energy-green rounded-full mt-2 animate-pulse"></div>
           </div>
