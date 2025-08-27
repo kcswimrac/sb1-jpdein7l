9d388c3b46cca470f2fa17f984a3fd2fc49acf74
kcswimrac <kcswimrac@gmail.com>
2025-08-26 21:02:58 -0700

    Update vite.config.ts

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/on-the-fly-energy-landing/',
+  base: '/sb1-jpdein7l/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
